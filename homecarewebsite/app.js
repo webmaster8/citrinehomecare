30176dd0f88a3ede919e25ed9fce4104370d711a
WebMaster <45870992+webmaster8@users.noreply.github.com>
2019-10-20 15:28:21 +0800

    all pages uploaded

--- homecarewebsite/app.js
+++ homecarewebsite/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const path = require('path');
 
 const app = express();
 
@@ -8,11 +9,28 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static', express.static('public'));
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-	res.render('index')
-})
+	res.render('index');
+});
+
+app.get('/homecare-services', (req, res) => {
+	res.render('homecare-services');
+});
+
+app.get('/family-support', (req, res) => {
+	res.render('family-support');
+});
+
+app.get('/hospital-to-home', (req, res) => {
+	res.render('hospital-to-home');
+});
+
+app.get('/care-resources', (req, res) => {
+	res.render('care-resources');
+});
 
 app.listen(8080, () => {
 	console.log('The application is running in port 8080!');
